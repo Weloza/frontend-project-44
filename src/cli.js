fbdea0be707f260bb29b03f648924d8aa480a81f
Weloza <danil557@icloud.com>
2023-08-26 18:13:42 +0400

    add src/ and src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const userGreeting = () => {
+	const name = readlineSync.question('May i have your name? ');
+	console.log('Hello, ' + name + '!');
+};
+export default userGreeting;
