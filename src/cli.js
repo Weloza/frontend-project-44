b0ab1ae8da72a9082df884a998bc73afc5e0c1dd
Weloza <danil557@icloud.com>
2023-10-19 16:12:41 +0400

    change brain-games.js and cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const userGreeting = () => {
+    console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May i have your name? ');
     console.log(`Hello, ${name}!`);
 };
