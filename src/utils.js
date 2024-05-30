8077ee87b0abb86762fc0727956fee9ce9a94d37
Weloza <danil557@icloud.com>
2024-05-30 12:35:52 +0400

    change tabulation utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export, arrow-body-style
 const getRandomInRange = (min = 0, max = 100) => {
-    return Math.floor(min + Math.random() * (max - min + 1));
+  return Math.floor(min + Math.random() * (max - min + 1));
 };
 
 export default getRandomInRange;
